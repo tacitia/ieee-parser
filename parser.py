91b60e8b9dfd22bc72f2d8e7037d4f8bf57d2240
Hong Zhang <Hong@Hongs-MacBook-Pro.local>
2015-12-17 16:08:06 -0500

    add complex parser

--- parser.py
+++ parser.py
@@ -1,7 +1,36 @@
 import xml.etree.ElementTree as ET
-import sys
+import sys, json
 from Entry import Entry
 from csv_writer import CSVWriter
 
+exampleEntry = {
+    'title': '',
+    'authors': '',
+    'abstract': '',
+    'affiliations': '',
+    'terms': [],
+    'pubId': 0,
+    'date': 2000
+}
 
-print('hello world!')
+tree = ET.parse('data/TVCGPubs1.xml')
+root = tree.getroot()
+# for child in root:
+#	for subchild in child:
+for child in root.findall('document'):
+	title = child.find('title').text
+	author = child.find('authors').text
+	abstract=child.find('abstract').text
+	#affiliations=child.find('affiliations').text
+	print(child.find('controlledterms').findall('term'))
+	#print(affiliations)
+	#abstract = child.find('abstract')
+	#affiliations = child.find('affiliations')
+	#for subchild in child:
+		#print(subchild)
+		#print(subchild.items())
+		#title = subchild.find('title')
+	#	print(subchild.tag)
+	#break
+
+
